3fd04e24c1c829a061682a93d0cba7a76f36a921
Romain Beauxis <romain.beauxis@gmail.com>
2026-07-01 13:57:54 -0500

    Bring back optional syslog.

---
+++ lib/log/log_printf.ml
@@ -0,0 +1,9 @@
+let init () = ()
+
+let log _level fmt =
+  Printf.ksprintf (fun msg -> Printf.eprintf "%s\n%!" msg) fmt
+
+let debug fmt = log `debug fmt
+let info fmt = log `info fmt
+let warn fmt = log `warn fmt
+let err fmt = log `err fmt
